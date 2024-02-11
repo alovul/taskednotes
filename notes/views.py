2c905efa3d8726f9779e9030566aaee56b139d3e
alovul <brandonfalk73@gmail.com>
2024-02-11 13:15:18 -0500

    Django Core

---
+++ notes/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from notes.forms import LogMessageForm
+from notes.models import LogMessage
+from django.utils.timezone import datetime
+from django.views.generic import ListView
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "notes/log_message.html", {"form": form})
